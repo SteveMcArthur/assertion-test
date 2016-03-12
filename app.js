352461b8341ae25a2483d9c1063e97913a161169
SteveMcArthur <contact@stevemcarthur.co.uk>
2016-03-12 11:22:33 +0000

    removed callbacks

--- app.js
+++ app.js
@@ -5,22 +5,16 @@ var assert = require("assert-helpers");
 joe.suite("Assertion tests", function (suite, test) {
 
 
-    test("This test should pass", function (done) {
+    test("This test should pass", function () {
         assert.equal(true, true, "I'm true");
-        done();
-
     });
 
-    test("This test should fail", function (done) {
+    test("This test should fail", function () {
         assert.equal(true, false, "I'm NOT true");
-        done();
-
     });
 
-    test("This test should run and pass", function (done) {
+    test("This test should run and pass", function () {
         assert.equal(1, 1, "I'm 1");
-        done();
-
     });
 
 });
